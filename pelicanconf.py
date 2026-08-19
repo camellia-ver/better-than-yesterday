ac4e4a60c2b39bfb4260c1e26abecb28e982c698
camellia-ver <jakahi435@gmail.com>
2026-08-19 12:56:15 +0900

    fix: 404 error

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,6 +1,6 @@
 AUTHOR = 'j'
-SITENAME = '어제보다 오늘 - '
-SITEURL = 'https://username.github.io/my-blog'
+SITENAME = '어제보다 오늘 - Better Than Yesterday'
+SITEURL = 'https://camellia-ve.github.io/my-blog'
 
 PATH = "content"
 
